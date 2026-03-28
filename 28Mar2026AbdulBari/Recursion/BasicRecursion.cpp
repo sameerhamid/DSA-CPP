a921a77dcf503717e16acd5dbc00f698e2190be1
Sameer <sameer@theastrotime.com>
2026-03-28 19:05:42 +0530

    print values from 1 to n

--- 28Mar2026AbdulBari/Recursion/BasicRecursion.cpp
+++ 28Mar2026AbdulBari/Recursion/BasicRecursion.cpp
@@ -8,8 +8,17 @@ void printValuesFromNTo1(int n) {
     }
 }
 
+void printValuesFrom1ToN(int n) {
+    if (n > 0) {
+        printValuesFrom1ToN(n - 1);
+        cout << n << " ";
+    }
+}
+
 int main() {
     cout << "-----------START------------" << endl;
     printValuesFromNTo1(5);
+    cout << endl;
+    printValuesFrom1ToN(5);
     cout << endl << "-----------END------------" << endl;
 }
