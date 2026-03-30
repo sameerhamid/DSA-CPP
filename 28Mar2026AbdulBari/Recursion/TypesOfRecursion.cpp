26968bd085f7e375c77797a979f86d8e08284ee7
Sameer <sameer@theastrotime.com>
2026-03-30 11:30:01 +0530

    learn about head recursion

--- 28Mar2026AbdulBari/Recursion/TypesOfRecursion.cpp
+++ 28Mar2026AbdulBari/Recursion/TypesOfRecursion.cpp
@@ -23,11 +23,37 @@ void tailRecursionUsingLoop(int n) {
 }
 // CONCLUION: If you to write tail recursion then better is to convert into a loop that more efficient in terms of space
 
+
+// ------------------------------------------------------------
+// 2. HEAD RECURSION:-> IF A RECURSIVE FUNCTION IS CALING ITSELF AND THAT RECURSIVE CALL IS THE FIRST STATEMENT IN A FUNCTION
+                    // The function does not have to procces or perform any operations at the time of calling  it has do everthing at the time of returning
+                    // All the processing is done at returning time
+void headRecursion(int n) {
+    if(n > 0) {
+        headRecursion(n - 1);
+        cout << n << " ";
+    }
+}
+
+void headRecursionUsingLoop(int n) {
+    int i = 1;
+    while (i <= n) {
+        cout << i << " ";
+        i++;
+    }
+}
+
+// IF THE RECURSIVE FUNCTION HAS TO DO SOMETHING AT RETURNING TIME , IT CAN NOT BE EASILY CONVERTED IN THE FORM OF THE LOOP BUT IT CAN BE CONVERTED
+
 int main() {
     cout << "-----------START------------" << endl;
     tailRecursion(5);
     cout << endl;
     tailRecursionUsingLoop(5);
+    cout << endl;
+    headRecursion(5);
+    cout << endl;
+    headRecursionUsingLoop(5);
     cout << endl;
     cout << endl << "-----------END------------" << endl;
     return 0;
