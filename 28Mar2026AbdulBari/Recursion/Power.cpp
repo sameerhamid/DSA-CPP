ad6914f10fd8267ce968edfd64e4546cd9ab9e51
Sameer <sameer@theastrotime.com>
2026-04-01 11:42:03 +0530

    add function to calculate power in optimal way

--- 28Mar2026AbdulBari/Recursion/Power.cpp
+++ 28Mar2026AbdulBari/Recursion/Power.cpp
@@ -8,12 +8,22 @@ int Power(int m, int n) {
     return Power(m, n-1) * m;
 }
 
-
+int PowerOptimal(int m, int n) {
+    if (n == 0) {
+        return 1;
+    }
+    if (n % 2 == 0) {
+        return PowerOptimal(m * m, n / 2);
+    }
+    return m * PowerOptimal(m * m, (n -1) / 2);
+}
 
 int main() {
     cout << "-----------START------------" << endl;
     cout << "Power: "<< Power(2, 5) << endl;
-
+    cout << endl;
+    cout << "PowerOptimal: "<< Power(3, 4) << endl;
+    cout << endl;
     cout << endl << "-----------END------------" << endl;
     return 0;
 }
