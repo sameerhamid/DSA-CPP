6f890a34a845c66e62641619b908a16adeeb8b86
Sameer <sameer@theastrotime.com>
2026-04-01 12:29:36 +0530

    add programe for taylor series

---
+++ 28Mar2026AbdulBari/Recursion/TaylorSeries.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+double e(int x, int n) {
+    static double p = 1, f = 1;
+    double r;
+    if (n == 0) {
+        return 1;
+    }
+    r = e(x, n -1);
+    p = p * x;
+    f = f * n;
+    return r + (p / f);
+}
+
+int main() {
+    cout << "-----------START------------" << endl;
+    cout << "TaylorSeries -------------- " << e(4, 15);
+    cout << endl;
+    cout << endl << "-----------END------------" << endl;
+    return 0;
+}
