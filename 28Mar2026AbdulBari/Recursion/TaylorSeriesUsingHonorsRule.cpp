d47d6f14645bd9941318e5bf83cef20d0d2fabfa
Sameer <sameer@theastrotime.com>
2026-04-02 12:16:22 +0530

    update caculation for float values

--- 28Mar2026AbdulBari/Recursion/TaylorSeriesUsingHonorsRule.cpp
+++ 28Mar2026AbdulBari/Recursion/TaylorSeriesUsingHonorsRule.cpp
@@ -4,7 +4,7 @@ using namespace std;
 double TaylorSeriesUsingLoop(int x, int n) {
     double S = 1;
     for(; n > 0; n--) {
-        S = (1 + (x / n)) * S;
+        S = 1 + x * S / n;
     }
     return S;
 }
@@ -14,7 +14,7 @@ double TaylorSeriesUsingRecursion(int x, int n) {
     if (n == 0) {
         return S;
     }
-    S = (1 + (x / n)) * S;
+    S = 1 + x * S / n;
     return TaylorSeriesUsingRecursion(x, n - 1);
 }
 
@@ -22,7 +22,7 @@ int main() {
     cout << "-----------START------------" << endl;
     cout << "TaylorSeriesUsingLoop -------------- " << TaylorSeriesUsingLoop(4, 15);
     cout << endl;
-    cout << "TaylorSeriesUsingRecursion -------------- " << TaylorSeriesUsingRecursion(4, 15);
+    cout << "TaylorSeriesUsingRecursion -------------- " << TaylorSeriesUsingRecursion(1, 10);
     cout << endl;
     cout << endl << "-----------END------------" << endl;
     return 0;
