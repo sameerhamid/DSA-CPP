12fa120af8512f59d4bffa091d25464136e76fa5
Sameer <sameer@theastrotime.com>
2026-04-02 12:13:00 +0530

    add progame for taylor using honors rule by recursive approach

--- 28Mar2026AbdulBari/Recursion/TaylorSeriesUsingHonorsRule.cpp
+++ 28Mar2026AbdulBari/Recursion/TaylorSeriesUsingHonorsRule.cpp
@@ -9,11 +9,20 @@ double TaylorSeriesUsingLoop(int x, int n) {
     return S;
 }
 
+double TaylorSeriesUsingRecursion(int x, int n) {
+    static double S = 1;
+    if (n == 0) {
+        return S;
+    }
+    S = (1 + (x / n)) * S;
+    return TaylorSeriesUsingRecursion(x, n - 1);
+}
 
 int main() {
     cout << "-----------START------------" << endl;
     cout << "TaylorSeriesUsingLoop -------------- " << TaylorSeriesUsingLoop(4, 15);
-
+    cout << endl;
+    cout << "TaylorSeriesUsingRecursion -------------- " << TaylorSeriesUsingRecursion(4, 15);
     cout << endl;
     cout << endl << "-----------END------------" << endl;
     return 0;
