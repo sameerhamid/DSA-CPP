1b4c11e950f56ec3bcf76da0df0ff8cfe17044fe
Sameer <sameer@theastrotime.com>
2026-04-02 12:22:40 +0530

    calcuate taylor series using itrative apporach

--- 28Mar2026AbdulBari/Recursion/TaylorSeries.cpp
+++ 28Mar2026AbdulBari/Recursion/TaylorSeries.cpp
@@ -14,9 +14,21 @@ double e(int x, int n) {
     return r + (p / f);
 }
 
+double TaylorSeriesItrative(int x, int n) {
+    double p = 1, f = 1;
+    double S = 1;
+    for (int i = 1; i <= n; i++) {
+        p = p * x;
+        f = (f * i);
+        S += p / f;
+    }
+    return S;
+}
 int main() {
     cout << "-----------START------------" << endl;
     cout << "TaylorSeries -------------- " << e(4, 15);
+    cout << endl;
+    cout << "TaylorSeriesItrative -------------- " << TaylorSeriesItrative(4, 15);
     cout << endl;
     cout << endl << "-----------END------------" << endl;
     return 0;
