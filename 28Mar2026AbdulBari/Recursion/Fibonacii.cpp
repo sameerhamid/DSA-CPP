b1cc3fb044a9952c42d748cf947ad823738ee17b
Sameer <sameer@theastrotime.com>
2026-04-06 18:47:11 +0530

    add fuction to calculate fibonacii number using itrative approach

--- 28Mar2026AbdulBari/Recursion/Fibonacii.cpp
+++ 28Mar2026AbdulBari/Recursion/Fibonacii.cpp
@@ -11,9 +11,27 @@ int FibonaciiNumber(int n) {
     return FibonaciiNumber(n - 2) + FibonaciiNumber(n -1);
 }
 
+int FibonaciiNumberItrative(int n) {
+    int t0 = 0, t1 = 1, s;
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+    for (int i = 2; i <= n; i++) {
+        s = t0 + t1;
+        t0 = t1;
+        t1 = s;
+    }
+    return s;
+}
+
 int main() {
     cout << "-----------START------------" << endl;
     cout << "FibonaciiNumber -------------- " << FibonaciiNumber(8);
+    cout << endl;
+    cout << "FibonaciiNumberItrative -------------- " << FibonaciiNumberItrative(8);
     cout << endl;
     cout << "-----------END------------" << endl;
     return 0;
