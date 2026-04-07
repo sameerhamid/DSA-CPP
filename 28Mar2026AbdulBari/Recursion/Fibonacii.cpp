b78a1f9b0128054269628b1e0cfc42e4214e5563
Sameer <sameer@theastrotime.com>
2026-04-07 11:20:02 +0530

    update condition

--- 28Mar2026AbdulBari/Recursion/Fibonacii.cpp
+++ 28Mar2026AbdulBari/Recursion/Fibonacii.cpp
@@ -13,11 +13,8 @@ int FibonaciiNumber(int n) {
 
 int FibonaciiNumberItrative(int n) {
     int t0 = 0, t1 = 1, s;
-    if (n == 0) {
-        return 0;
-    }
-    if (n == 1) {
-        return 1;
+    if (n <= 1) {
+        return n;
     }
     for (int i = 2; i <= n; i++) {
         s = t0 + t1;
