f7b6d9a03bb6e98f5cb2f017119508b60dc58838
Sameer <sameer@theastrotime.com>
2026-04-13 11:03:03 +0530

    calculate NCR using recursion

--- 28Mar2026AbdulBari/Recursion/NcR.cpp
+++ 28Mar2026AbdulBari/Recursion/NcR.cpp
@@ -15,9 +15,17 @@ int CalculateNcR(int n, int r) {
     return nFact / (rFact * nMinusRFact);
 }
 
+int NCR(int n, int r) {
+    if (n == r || r == 0) {
+        return 1;
+    }
+    return NCR(n-1, r-1) + NCR(n-1, r);
+}
+
 int main() {
     cout << "-----------START------------" << endl;
     cout << "CalculateNcR>>>>>>>> "<< CalculateNcR(5, 3) << endl;
+    cout << "CalculateNcR>>>>>>>> "<< NCR(5, 3) << endl;
     cout << endl << "-----------END------------" << endl;
     return 1;
 }
