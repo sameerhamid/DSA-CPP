f13a8a7386a3493a18f88360093432f6952325e1
Sameer <sameer@theastrotime.com>
2026-04-30 11:56:33 +0530

    add function to toggle cases

--- 28Mar2026AbdulBari/Strings/ChangeCases.cpp
+++ 28Mar2026AbdulBari/Strings/ChangeCases.cpp
@@ -1,19 +1,36 @@
 #include<iostream>
 using namespace std;
 
-void changeToLowercase(char str[]) {
-    for(int i = 0; str[i]!='\0'; i++) {
-        if (str[i] < 90) {
+void changeToLowercase(char str[])
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] < 90)
+        {
             str[i] = str[i] + 32;
         }
     }
     cout << str << endl;
 }
 
+void changeUperToLowerLowerToUpper(char str[])
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] <= 90 && str[i] >= 65)
+        {
+            str[i] = str[i] + 32;
+        } else if (str[i] <=122 && str[i] >= 97) {
+            str[i] = str[i] - 32;
+        }
+    }
+    cout << str << endl;
+}
+
 int main() {
-    char str[] = "SAmEER";
+    char str[] = "SaMeeR";
     int len = 0;
-    changeToLowercase(str);
+    changeUperToLowerLowerToUpper(str);
     cout << endl;
     return 0;
 }
