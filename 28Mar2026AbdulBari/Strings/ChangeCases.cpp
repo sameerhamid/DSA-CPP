e4488187ed408589be94da5dc42e22df6813d1a5
Sameer <sameer@theastrotime.com>
2026-04-30 12:00:19 +0530

    format

--- 28Mar2026AbdulBari/Strings/ChangeCases.cpp
+++ 28Mar2026AbdulBari/Strings/ChangeCases.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 void changeToLowercase(char str[])
@@ -20,14 +20,17 @@ void changeUperToLowerLowerToUpper(char str[])
         if (str[i] <= 90 && str[i] >= 65)
         {
             str[i] = str[i] + 32;
-        } else if (str[i] <=122 && str[i] >= 97) {
+        }
+        else if (str[i] <= 122 && str[i] >= 97)
+        {
             str[i] = str[i] - 32;
         }
     }
     cout << str << endl;
 }
 
-int main() {
+int main()
+{
     char str[] = "SaMeeR";
     int len = 0;
     changeUperToLowerLowerToUpper(str);
