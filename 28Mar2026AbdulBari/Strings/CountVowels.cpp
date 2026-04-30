7389ace5bb6c53e45d71494e0919134ee200e893
Sameer <sameer@theastrotime.com>
2026-04-30 12:18:01 +0530

    update function to count both vowels and consonants

--- 28Mar2026AbdulBari/Strings/CountVowels.cpp
+++ 28Mar2026AbdulBari/Strings/CountVowels.cpp
@@ -1,21 +1,28 @@
 #include<iostream>
 using namespace std;
 
-int countVowels(char str[]) {
-    int vCount = 0;
+
+pair<int, int> countVowelsConsonants(char str[]) {
+    int vCount = 0, cCount = 0;
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' ||
             str[i] == 'o' || str[i] == 'u' ||
             str[i] == 'A' || str[i] == 'E' || str[i] == 'I' ||
             str[i] == 'O' || str[i] == 'U') {
             vCount++;
+        } else {
+            if ((str[i] >= 90 && str[i] <= 65) || (str[i] >= 97 && str[i] <= 122)) {
+                cCount++;
+            }
         }
     }
-    return vCount;
+    return {vCount, cCount};
 }
 
 int main() {
     char str[] = "Sameer";
-    cout << "Vowels " << countVowels(str) << endl;
+    pair<int, int> result = countVowelsConsonants(str);
+    cout << "Vowels: " << result.first << endl;
+    cout << "Consonants: " << result.second << endl;
     return 0;
 }
