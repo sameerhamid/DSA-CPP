6de537358f9cabcce158b31b6795a1bd6d6e4af4
Sameer <sameer@theastrotime.com>
2026-05-04 12:03:20 +0530

    add function to reverse a string

---
+++ 28Mar2026AbdulBari/Strings/ReverseString.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+void Reverse(char str[]) {
+    int i, j;
+    for (j = 0; str[j] != '\0'; j++);
+    j = j - 1;
+    for (i = 0; i < j; i++, j--) {
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+}
+
+int main() {
+    char str[] = "Sameer is a great guy";
+    Reverse(str);
+    for (int i = 0; str[i] != '\0'; i++) {
+        cout << str[i];
+    }
+    cout << endl;
+    return 0;
+}
